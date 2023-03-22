dc710110d9413c8685574643d0097a16b885679c
Justice Orogun <jay@justiceorogun.com>
2023-03-22 15:00:03 +0100

    I made some changes to the about, food menu and other sections

--- script.js
+++ script.js
@@ -23,10 +23,10 @@ for (let i = 0; i < navbarLinks.length; i++){
 }
 
 
-
+      // This is for the header section
 
 const header = document.querySelector("[data-header]"); 
-
+const backTopBtn = document.querySelector("[data-back-top-btn]"); 
 
 window.addEventListener("scroll", function() {
   if (window.scrollY >=100) {
@@ -34,10 +34,11 @@ window.addEventListener("scroll", function() {
   backTopBtn.classList.add("active"); 
 }else{ 
   header.classList.remove("active"); 
+  backTopBtn.classList.remove("active"); 
 }
 }); 
 
-
+        // This is for the search bar
 
 const searchBtn = document.querySelector("[data-search-btn]"); 
 const searchContainer = document.querySelector("[data-search-container]"); 
@@ -53,3 +54,31 @@ for (let i = 0; 1 < searchBoxElems.length; i++) {
     document.body.classList.toggle("active"); 
   }); 
 }
+
+
+      // This is for the deliveryboy 
+
+const deliveryBoy = document.querySelector("[data-delivery-boy]"); 
+
+let deliveryBoyMove = -80; 
+let lastScrollPos = 0; 
+
+window.addEventListener("scroll", function () {
+  let deliveryBoyTopPos = deliveryBoy.getBoundingClientRect().top; 
+
+  if(deliveryBoyTopPos < 500 && deliveryBoyTopPos > -250) { 
+    let activeScrollPos = window.scrollY; 
+
+    if (lastScrollPos < activeScrollPos) {
+      deliveryBoyMove += 1; 
+    }else{ 
+      deliveryBoyMove -= 1; 
+    }
+
+    lastScrollPos = activeScrollPos; 
+    deliveryBoy.style.transform = `translateX(${deliveryBoyMove})`; 
+  }
+}); 
+
+
+
